1ab605b2084c450f93e8ebe0876c824bf277b56e
Narutchai Sukkeha <5710451223@bitbucket.org>
2017-05-09 02:39:30 +0700

    show typeClass

--- resources/views/typeClass/index.blade.php
+++ resources/views/typeClass/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container-fluid">
-    <h1>Our Classes</h1>
+    <h1>All Classes</h1>
 
 </div>
 <div class="card-deck" id = 'vue-app-typeClass'>
@@ -10,7 +10,7 @@
 
       <div class="col-sm-4">
   <div class="card">
-    <img class="card-img-top" src="/images/discount{{$d->id}}.jpg" style="width:338px;height:170px;">
+    <img class="card-img-top" src="/images/cls{{$d->id}}.jpg" style="width:338px;height:170px;">
     <div class="card-block">
       <h4 class="card-title">{{$d->name}}</h4>
       <p class="card-text">{{$d->description}}</p>
