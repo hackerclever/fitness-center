2f4c65e6051aeca6f2d63bb7deb5d1b793f3ca75
Narutchai Sukkeha <5710451223@bitbucket.org>
2017-05-08 16:53:46 +0700

    welcome.blade link to showPromotion

---
+++ resources/views/promotions/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container-fluid">
+    <h1>Promotion</h1>
+
+</div>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,8 @@
         <a href="#" class="thumbnail">
           <img src="https://unsplash.it/200?image=1" alt="Sample Image">
         </a>
+        <br>
+        <a href="{{ url('/promotion/show') }}">Promotion</a>
       </div>
       <div class="col-xs-6 col-md-3">
         <a href="#" class="thumbnail">
