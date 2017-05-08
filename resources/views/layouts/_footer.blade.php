5c314a4bd203e892cf16e17c34cc9b8a0c4f7962
Narutchai Sukkeha <5710451223@bitbucket.org>
2017-05-08 19:48:09 +0700

    modify welcome,promotion

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,10 @@
 <footer class="footer">
    <div class="container">
-     <h4>ABOUT US</h4>
-     <a href="{{ url('/contactUs/index') }}">Contact us</a><br>
+     <div class="d-inline-block">
+       <h4>ABOUT US</h4>
+       <a href="{{ url('/contactUs/index') }}">Contact us</a>
+    </div>
+<br><br>
      <span class="text-muted">&copy; Company 2017</span>
    </div>
  </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,8 @@
   <div class="section-inner">
     <h2>HIGHLIGHTS</h2>
     <p>Looking for a fitness center in Thailand? You've come to the right place as our centers are equipped with advanced facilities that cater to all your needs and more. Join our fitness workout center in Singapore today and experience workouts like never before. The journey to a new, healthier you awaits.</p>
+    <p><strong>Reasons To Join Fitness First</strong></p>
+    <p>Make every movement count. With just the right amount of training and motivation, we can help you reach your fitness goals. Fully furnished gyms and specially crafted exercise routines such as our Dynamic Movement Training will provide a constant challenge to better yourself.  </p>
   </div>
 
 </div>
