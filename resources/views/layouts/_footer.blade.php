29d3027de402c19588aa29f7117554908334b0d3
Narutchai Sukkeha <5710451223@bitbucket.org>
2017-05-08 18:10:30 +0700

    modify footer, welcome.blade

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,7 @@
 <footer class="footer">
    <div class="container">
+     <h4>ABOUT US</h4>
+     <a href="{{ url('/contactUs/index') }}">Contact us</a><br>
      <span class="text-muted">&copy; Company 2017</span>
    </div>
  </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,34 +3,14 @@
 @section('content')
     <div class="jumbotron">
         <div class="container">
-            <h1 class="text-center">Fitness</h1>
+            <h1 class="text-center">Fitness Center</h1>
         </div>
     </div>
 
     <div class="row">
-      <div class="col-xs-6 col-md-3">
-        <a href="#" class="thumbnail">
-          <img src="https://unsplash.it/200?image=0" alt="Sample Image">
-        </a>
-        <br>
-        <a href="{{ url('/contactUs/index') }}">contact</a>
-      </div>
-      <div class="col-xs-6 col-md-3">
-        <a href="#" class="thumbnail">
-          <img src="https://unsplash.it/200?image=1" alt="Sample Image">
-        </a>
-        <br>
-        <a href="{{ url('/promotion/show') }}">Promotion</a>
-      </div>
-      <div class="col-xs-6 col-md-3">
-        <a href="#" class="thumbnail">
-          <img src="https://unsplash.it/200?image=2" alt="Sample Image">
-        </a>
-      </div>
-      <div class="col-xs-6 col-md-3">
-        <a href="#" class="thumbnail">
-          <img src="https://unsplash.it/200?image=3" alt="Sample Image">
-        </a>
-      </div>
+      <ul>
+        <li><a href="{{ url('/contactUs/index') }}">contact</a></li>
+        <li><a href="{{ url('/promotion/show') }}">Promotion</a></li>
+      </ul>
     </div>
 @endsection
