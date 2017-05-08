8b5e501ac0363fcf9c3682db69419d000dd8b4bd
Narutchai Sukkeha <5710451223@bitbucket.org>
2017-05-08 17:29:11 +0700

    modify contactUs

--- resources/views/contactUs/index.blade.php
+++ resources/views/contactUs/index.blade.php
@@ -3,9 +3,34 @@
 @section('content')
     <div class="jumbotron">
         <div class="container">
-            <h1 class="text-center">Contact us</h1>
+            <h1 class="text" style="color:brown;">Contact us</h1>
         </div>
     </div>
+    <div class="row">
+          <div class="col-xs-6 col-sm-4">
+            <h4>PLATINUM </h4>
+            <h4>LANDMARK PLAZA</h4>
+            <label>4th Fl. Landmark Plaza, 138</label>
+            <br>
+            <label>Sukhumvit Road, Klongtoey</label>
+            <br>
+            <label>Bangkok</label>
+            <br>
+            <label>10110</label>
+            <br>
+          </div>
+          <div class="col-xs-4 col-sm-6">
+            <label>Openning times</label>
+            <br><br>
+            <label>Monday to Friday</label>
+            <br>
+            <label>6:00AM - 10:00PM</label>
+            <br><br>
+            <label>Weekend</label>
+            <br>
+            <label>8:00AM - 9:00PM</label>
+          </div>
+        </div>
 
 
 @endsection
